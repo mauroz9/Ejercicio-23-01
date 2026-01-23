b26f24646827a0febaa36928d28188d238dbed15
mauroz9 <mauro.serranoo26@gmail.com>
2026-01-23 09:10:55 +0100

    Iniciación del proyecto

---
+++ src/main/java/com/salesianostriana/dam/viviendafilter/ViviendafilterApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.viviendafilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViviendafilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViviendafilterApplication.class, args);
+	}
+
+}
